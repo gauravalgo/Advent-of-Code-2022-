ec853f9c8989bb125525c1d646bc20cb6662c8dd
gauravalgo <opengalgogames@gmail.com>
2023-06-13 19:00:08 +0530

    latest

--- src/bin/01.rs
+++ src/bin/01.rs
@@ -2,17 +2,20 @@ use itertools::Itertools;
 use std::cmp::Reverse;
 
 type Input = Vec<u32>;
+
 fn parse(input: &str) -> Input {
-    input.split("\n\n")
-        .map(|c| c.lines().find_map(|l| l.parse::<u32>.Ok()).sum())
+    input
+        .split("\n\n")
+        .map(|c| c.lines().filter_map(|l| l.parse::<u32>().ok()).sum())
         .collect()
 }
-pub fn part_one(calorie_counts: Input) -> Option<u32> {
-    calorie_counts.iter().max().copied()
+
+pub fn part_one(input: Input) -> Option<u32> {
+    input.iter().max().copied()
 }
 
-pub fn part_two(calorie_counts: Input) -> Option<u32> {
-    Some(calorie_counts
+pub fn part_two(input: Input) -> Option<u32> {
+    Some(input
          .iter()
          .sorted_by_key(|x| Reverse(*x))
          .take(3)
@@ -20,12 +23,7 @@ pub fn part_two(calorie_counts: Input) -> Option<u32> {
          )
 }
 
-
-fn main() {
-    let input = &advent_of_code::read_file("inputs", 1);
-    advent_of_code::solve!(1, part_one, input);
-    advent_of_code::solve!(2, part_two, input);
-}
+advent_of_code::main!(1);
 
 #[cfg(test)]
 mod tests {
@@ -33,14 +31,13 @@ mod tests {
 
     #[test]
     fn test_part_one() {
-        //let input = advent_of_code::read_file("examples", 1);
-        let result = part_one(parse(&advent_of_code::read_file("examples", 1)));
+        let result = part_one(parse(&advent_of_code::template::read_file("examples", 1)));
         assert_eq!(result, Some(24000));
     }
 
     #[test]
     fn test_part_two() {
-        let result = part_two(parse(&advent_of_code::read_file("examples",2)));
-        assert_eq!(part_two(&input), None);
+        let result = part_two(parse(&advent_of_code::template::read_file("examples", 1)));
+        assert_eq!(result, Some(45000));
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,234 +1,390 @@
-/*
- * This file contains template code.
- * There is no need to edit this file unless you want to change template functionality.
- * Prefer `./helpers.rs` if you want to extract code from your solutions.
- */
-use std::env;
-use std::fs;
+use hashbrown::HashMap;
+use std::{cmp, slice::Iter};
 
-pub mod helpers;
+pub mod template;
 
-pub const ANSI_ITALIC: &str = "\x1b[3m";
-pub const ANSI_BOLD: &str = "\x1b[1m";
-pub const ANSI_RESET: &str = "\x1b[0m";
+#[derive(Clone, Debug)]
+pub enum Direction {
+    North,
+    NorthEast,
+    East,
+    SouthEast,
+    South,
+    SouthWest,
+    West,
+    NorthWest,
+}
 
-#[macro_export]
-macro_rules! solve {
-    ($part:expr, $solver:ident, $input:expr) => {{
-        use advent_of_code::{ANSI_BOLD, ANSI_ITALIC, ANSI_RESET};
-        use std::fmt::Display;
-        use std::time::Instant;
+impl Direction {
+    pub fn all() -> Iter<'static, Direction> {
+        static DIRECTIONS: [Direction; 8] = [
+            Direction::North,
+            Direction::NorthEast,
+            Direction::East,
+            Direction::SouthEast,
+            Direction::South,
+            Direction::SouthWest,
+            Direction::West,
+            Direction::NorthWest,
+        ];
 
-        fn print_result<T: Display>(func: impl FnOnce(&str) -> Option<T>, input: &str) {
-            let timer = Instant::now();
-            let result = func(input);
-            let elapsed = timer.elapsed();
-            match result {
-                Some(result) => {
-                    println!(
-                        "{} {}(elapsed: {:.2?}){}",
-                        result, ANSI_ITALIC, elapsed, ANSI_RESET
-                    );
-                }
-                None => {
-                    println!("not solved.")
-                }
-            }
+        DIRECTIONS.iter()
+    }
+}
+
+/// A point in a 2D grid.
+/// Uses `isize` to support use in sparse grids where point indexes may be negative.
+#[derive(Clone, Debug, Eq, Hash, PartialEq)]
+pub struct Point {
+    pub x: isize,
+    pub y: isize,
+}
+
+impl Point {
+    pub fn manhattan_distance(&self, other: &Point) -> isize {
+        (self.x - other.x).abs() + (self.y - other.y).abs()
+    }
+
+    pub fn chebyshev_distance(&self, other: &Point) -> isize {
+        cmp::max((self.x - other.x).abs(), (self.y - other.y).abs())
+    }
+
+    /// Get point x steps away in a given direction.
+    #[inline(always)]
+    pub fn get_neighbour(&self, direction: &Direction, steps: isize) -> Self {
+        match direction {
+            Direction::North => Self {
+                x: self.x,
+                y: self.y - steps,
+            },
+            Direction::NorthEast => Self {
+                x: self.x - steps,
+                y: self.y - steps,
+            },
+            Direction::East => Self {
+                x: self.x - steps,
+                y: self.y,
+            },
+            Direction::SouthEast => Self {
+                x: self.x - steps,
+                y: self.y + steps,
+            },
+            Direction::South => Self {
+                x: self.x,
+                y: self.y + steps,
+            },
+            Direction::SouthWest => Self {
+                x: self.x + steps,
+                y: self.y + steps,
+            },
+            Direction::West => Self {
+                x: self.x + steps,
+                y: self.y,
+            },
+            Direction::NorthWest => Self {
+                x: self.x + steps,
+                y: self.y - steps,
+            },
+        }
+    }
+
+    #[inline(always)]
+    pub fn get_direction(&self, target: &Point) -> Option<Direction> {
+        if self == target {
+            return None;
         }
 
-        println!("🎄 {}Part {}{} 🎄", ANSI_BOLD, $part, ANSI_RESET);
-        print_result($solver, $input);
-    }};
-}
-
-pub fn read_file(folder: &str, day: u8) -> String {
-    let cwd = env::current_dir().unwrap();
-
-    let filepath = cwd.join("src").join(folder).join(format!("{day:02}.txt"));
-
-    let f = fs::read_to_string(filepath);
-    f.expect("could not open input file")
-}
-
-fn parse_time(val: &str, postfix: &str) -> f64 {
-    val.split(postfix).next().unwrap().parse().unwrap()
-}
-
-pub fn parse_exec_time(output: &str) -> f64 {
-    output.lines().fold(0_f64, |acc, l| {
-        if !l.contains("elapsed:") {
-            acc
-        } else {
-            let timing = l.split("(elapsed: ").last().unwrap();
-            // use `contains` istd. of `ends_with`: string may contain ANSI escape sequences.
-            // for possible time formats, see: https://github.com/rust-lang/rust/blob/1.64.0/library/core/src/time.rs#L1176-L1200
-            if timing.contains("ns)") {
-                acc // range below rounding precision.
-            } else if timing.contains("µs)") {
-                acc + parse_time(timing, "µs") / 1000_f64
-            } else if timing.contains("ms)") {
-                acc + parse_time(timing, "ms")
-            } else if timing.contains("s)") {
-                acc + parse_time(timing, "s") * 1000_f64
+        let direction = if self.x == target.x {
+            if self.y < target.y {
+                Direction::South
             } else {
-                acc
+                Direction::North
             }
-        }
-    })
-}
-
-/// copied from: https://github.com/rust-lang/rust/blob/1.64.0/library/std/src/macros.rs#L328-L333
-#[cfg(test)]
-macro_rules! assert_approx_eq {
-    ($a:expr, $b:expr) => {{
-        let (a, b) = (&$a, &$b);
-        assert!(
-            (*a - *b).abs() < 1.0e-6,
-            "{} is not approximately equal to {}",
-            *a,
-            *b
-        );
-    }};
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_parse_exec_time() {
-        assert_approx_eq!(
-            parse_exec_time(&format!(
-                "🎄 Part 1 🎄\n0 (elapsed: 74.13ns){}\n🎄 Part 2 🎄\n0 (elapsed: 50.00ns){}",
-                ANSI_RESET, ANSI_RESET
-            )),
-            0_f64
-        );
-
-        assert_approx_eq!(
-            parse_exec_time("🎄 Part 1 🎄\n0 (elapsed: 755µs)\n🎄 Part 2 🎄\n0 (elapsed: 700µs)"),
-            1.455_f64
-        );
-
-        assert_approx_eq!(
-            parse_exec_time("🎄 Part 1 🎄\n0 (elapsed: 70µs)\n🎄 Part 2 🎄\n0 (elapsed: 1.45ms)"),
-            1.52_f64
-        );
-
-        assert_approx_eq!(
-            parse_exec_time(
-                "🎄 Part 1 🎄\n0 (elapsed: 10.3s)\n🎄 Part 2 🎄\n0 (elapsed: 100.50ms)"
-            ),
-            10400.50_f64
-        );
-    }
-}
-
-pub mod aoc_cli {
-    use std::{
-        fmt::Display,
-        fs::create_dir_all,
-        process::{Command, Output, Stdio},
-    };
-
-    pub enum AocCliError {
-        CommandNotFound,
-        CommandNotCallable,
-        BadExitStatus(Output),
-        IoError,
-    }
-
-    impl Display for AocCliError {
-        fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-            match self {
-                AocCliError::CommandNotFound => write!(f, "aoc-cli is not present in environment."),
-                AocCliError::CommandNotCallable => write!(f, "aoc-cli could not be called."),
-                AocCliError::BadExitStatus(_) => {
-                    write!(f, "aoc-cli exited with a non-zero status.")
-                }
-                AocCliError::IoError => write!(f, "could not write output files to file system."),
+        } else if self.y == target.y {
+            if self.x < target.x {
+                Direction::West
+            } else {
+                Direction::East
             }
-        }
-    }
-
-    pub fn check() -> Result<(), AocCliError> {
-        Command::new("aoc")
-            .arg("-V")
-            .output()
-            .map_err(|_| AocCliError::CommandNotFound)?;
-        Ok(())
-    }
-
-    pub fn read(day: u8, year: Option<u16>) -> Result<Output, AocCliError> {
-        // TODO: output local puzzle if present.
-        let args = build_args("read", &[], day, year);
-        call_aoc_cli(&args)
-    }
-
-    pub fn download(day: u8, year: Option<u16>) -> Result<Output, AocCliError> {
-        let input_path = get_input_path(day);
-
-        let puzzle_path = get_puzzle_path(day);
-        create_dir_all("src/puzzles").map_err(|_| AocCliError::IoError)?;
-
-        let args = build_args(
-            "download",
-            &[
-                "--overwrite".into(),
-                "--input-file".into(),
-                input_path.to_string(),
-                "--puzzle-file".into(),
-                puzzle_path.to_string(),
-            ],
-            day,
-            year,
-        );
-
-        let output = call_aoc_cli(&args)?;
-
-        if output.status.success() {
-            println!("---");
-            println!("🎄 Successfully wrote input to \"{}\".", &input_path);
-            println!("🎄 Successfully wrote puzzle to \"{}\".", &puzzle_path);
-            Ok(output)
+        } else if self.y < target.y {
+            if self.x < target.x {
+                Direction::NorthWest
+            } else {
+                Direction::NorthEast
+            }
+        } else if self.x < target.x {
+            Direction::SouthWest
         } else {
-            Err(AocCliError::BadExitStatus(output))
+            Direction::SouthEast
+        };
+
+        Some(direction)
+    }
+
+    pub fn line_to(&self, target: &Point) -> Vec<Point> {
+        let mut points = vec![self.clone()];
+
+        match self.get_direction(target) {
+            None => points,
+            Some(dir) => {
+                let mut cursor = self.clone();
+                loop {
+                    cursor = cursor.get_neighbour(&dir, 1);
+                    points.push(cursor.clone());
+                    if &cursor == target {
+                        break;
+                    }
+                }
+
+                points
+            }
+        }
+    }
+}
+
+/// Sparse grid where points may not exist at creation, or be negative.
+#[derive(Clone, Debug)]
+pub struct SparseGrid<T> {
+    pub points: HashMap<Point, T>,
+}
+
+impl<T> SparseGrid<T> {
+    pub fn get(&self, point: &Point) -> Option<&T> {
+        self.points.get(point)
+    }
+
+    pub fn insert(&mut self, point: Point, data: T) {
+        self.points.insert(point, data);
+    }
+}
+
+impl<T> Default for SparseGrid<T> {
+    fn default() -> Self {
+        Self {
+            points: HashMap::new(),
+        }
+    }
+}
+
+/// Simple 2D grid where each point maps to a value.
+#[derive(Clone, Debug)]
+pub struct SimpleGrid<T> {
+    pub width: usize,
+    pub height: usize,
+    pub data: Vec<Vec<T>>,
+}
+
+/// Simple 2D grid that expects to hold a value at each point.
+impl<T> SimpleGrid<T> {
+    /// Create a grid holding items of type T from a string representation.
+    /// Parser is called with (char, x, y).
+    pub fn from_str(input: &str, parse: &mut dyn FnMut(char, usize, usize) -> T) -> Self {
+        // map lines into a nested list, applying parser to each item.
+        let data: Vec<Vec<T>> = input
+            .trim()
+            .lines()
+            .enumerate()
+            .map(|(y, l)| l.chars().enumerate().map(|(x, c)| parse(c, x, y)).collect())
+            .collect();
+
+        SimpleGrid {
+            width: data[0].len(),
+            height: data.len(),
+            data,
         }
     }
 
-    fn get_input_path(day: u8) -> String {
-        let day_padded = format!("{day:02}");
-        format!("src/inputs/{day_padded}.txt")
+    /// Get a reference to the value for a certain point in the grid.
+    /// NOTE: unchecked.
+    pub fn get(&self, point: &Point) -> &T {
+        &self.data[point.y as usize][point.x as usize]
     }
 
-    fn get_puzzle_path(day: u8) -> String {
-        let day_padded = format!("{day:02}");
-        format!("src/puzzles/{day_padded}.md")
-    }
+    /// Get all points in grid.
+    pub fn points(&self) -> Vec<Point> {
+        let mut points = vec![];
 
-    fn build_args(command: &str, args: &[String], day: u8, year: Option<u16>) -> Vec<String> {
-        let mut cmd_args = args.to_vec();
-
-        if let Some(year) = year {
-            cmd_args.push("--year".into());
-            cmd_args.push(year.to_string());
+        for y in 0..self.height {
+            for x in 0..self.width {
+                points.push(Point {
+                    x: x as isize,
+                    y: y as isize,
+                })
+            }
         }
 
-        cmd_args.append(&mut vec!["--day".into(), day.to_string(), command.into()]);
-
-        cmd_args
+        points
     }
 
-    fn call_aoc_cli(args: &[String]) -> Result<Output, AocCliError> {
-        if cfg!(debug_assertions) {
-            println!("Calling >aoc with: {}", args.join(" "));
+    /// Check if a point is inside the grid.
+    pub fn is_inside(&self, point: &Point) -> bool {
+        point.x >= 0
+            && point.y >= 0
+            && (point.x as usize) < self.width
+            && (point.y as usize) < self.height
+    }
+
+    /// Check if a point is on the boundary of the grid.
+    pub fn is_boundary(&self, point: &Point) -> bool {
+        point.x == 0
+            || point.y == 0
+            || point.x as usize == self.width - 1
+            || point.y as usize == self.height - 1
+    }
+
+    /// Get a unique identifier for a point in this grid.
+    pub fn id_for_point(&self, p: &Point) -> usize {
+        p.x as usize + self.width * p.y as usize
+    }
+
+    /// Get underlying point for a unique idenfitier in this grid.
+    pub fn point_for_id(&self, id: usize) -> Point {
+        Point {
+            x: (id % self.width) as isize,
+            y: (id / self.width) as isize,
+        }
+    }
+
+    pub fn walk<'a>(&'a self, current: &'a Point, direction: &'a Direction) -> WalkingIterator<T> {
+        WalkingIterator {
+            current: current.clone(),
+            grid: self,
+            direction,
+        }
+    }
+}
+
+/// An iterator that: starting at point `p`, walks to the edge of the grid in direction `d`.
+pub struct WalkingIterator<'a, T> {
+    current: Point,
+    direction: &'a Direction,
+    grid: &'a SimpleGrid<T>,
+}
+
+impl<T> Iterator for WalkingIterator<'_, T> {
+    type Item = Point;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let next_point = self.current.get_neighbour(self.direction, 1);
+
+        if next_point.x < 0
+            || next_point.y < 0
+            || next_point.x as usize > self.grid.width - 1
+            || next_point.y as usize > self.grid.height - 1
+        {
+            None
+        } else {
+            self.current = next_point;
+            Some(self.current.clone())
+        }
+    }
+}
+
+/// Implementation odf dijkstra's algorithm.
+pub mod shortest_path {
+    use std::cmp::Ordering;
+    use std::collections::BinaryHeap;
+
+    use crate::{Direction, Point, SimpleGrid};
+
+    // while performing the search, track a sorted list of candidates (=state) to visit next on a priority queue.
+    #[derive(Copy, Clone, Eq, PartialEq)]
+    struct State {
+        cost: usize,
+        position: usize,
+    }
+
+    /// the algorithm expects a `min-heap` priority queue as frontier.
+    /// the default std. lib implementation is a `max-heap`, so the sort order needs to be flipped for state values.
+    /// also adds a tie breaker based on position. see [rust docs](https://doc.rust-lang.org/std/collections/struct.BinaryHeap.html#min-heap)
+    impl Ord for State {
+        fn cmp(&self, other: &Self) -> Ordering {
+            other
+                .cost
+                .cmp(&self.cost)
+                .then_with(|| self.position.cmp(&other.position))
+        }
+    }
+
+    impl PartialOrd for State {
+        fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+            Some(self.cmp(other))
+        }
+    }
+
+    static DIRECTIONS: [Direction; 4] = [
+        Direction::North,
+        Direction::South,
+        Direction::East,
+        Direction::West,
+    ];
+
+    pub fn shortest_path<T>(
+        grid: &SimpleGrid<T>,
+        start_points: &[Point],
+        end_point: &Point,
+        calc_cost: impl Fn(&Point) -> usize,
+        filter_neighbour: impl Fn(&Point, &Point) -> bool,
+    ) -> Option<usize> {
+        // dist[node] = current shortest distance from `start` to `node`.
+        let mut dist: Vec<_> = (0..(grid.width * grid.height))
+            .map(|_| usize::MAX)
+            .collect();
+        let mut frontier = BinaryHeap::new();
+
+        // initialize each start point with a zero cost and push it to frontier.
+        for start_point in start_points {
+            let start_id = grid.id_for_point(start_point);
+            dist[start_id] = 0;
+            frontier.push(State {
+                cost: 0,
+                position: start_id,
+            });
         }
 
-        Command::new("aoc")
-            .args(args)
-            .stdout(Stdio::inherit())
-            .stderr(Stdio::inherit())
-            .output()
-            .map_err(|_| AocCliError::CommandNotCallable)
+        let end_id = grid.id_for_point(end_point);
+
+        // examine the frontier starting with the lowest cost nodes.
+        while let Some(State { cost, position }) = frontier.pop() {
+            if position == end_id {
+                return Some(cost);
+            }
+
+            // skip: there is a better path to this node already.
+            if cost > dist[position] {
+                continue;
+            }
+
+            let current = grid.point_for_id(position);
+
+            let neighbours: Vec<Point> = DIRECTIONS
+                .iter()
+                .filter_map(|dir| {
+                    let neighbour = current.get_neighbour(dir, 1);
+
+                    if grid.is_inside(&neighbour) && filter_neighbour(&current, &neighbour) {
+                        Some(neighbour)
+                    } else {
+                        None
+                    }
+                })
+                .collect();
+
+            // see if we can find a path with a lower cost than previous paths for any adjacent nodes.
+            for neighbour in neighbours {
+                let next = State {
+                    cost: cost + calc_cost(&neighbour),
+                    position: grid.id_for_point(&neighbour),
+                };
+
+                // if so, add it to the frontier and continue.
+                if next.cost < dist[next.position] {
+                    frontier.push(next);
+                    dist[next.position] = next.cost;
+                }
+            }
+        }
+
+        None
     }
 }
